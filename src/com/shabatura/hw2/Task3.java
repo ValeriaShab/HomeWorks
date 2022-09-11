7cf7f55ec65ebd7db8d42b678478adce210e2a9e
ValeriaShab <sabaturavaleria@gmail.com>
2022-09-11 16:41:38 +0300

    hw3

--- src/com/shabatura/hw2/Task3.java
+++ src/com/shabatura/hw2/Task3.java
@@ -6,9 +6,12 @@ public class Task3 {
     }
 
     private static void calcLeast(int num1, int num2, int num3) {
-        if ((Math.abs(num1)) < (Math.abs(num2)) && (Math.abs(num1)) < (Math.abs(num3))) {
+        int absNum1 = (num1 < 0) ? -num1 : num1;
+        int absNum2 = (num2 < 0) ? -num2 : num2;
+        int absNum3 = (num3 < 0) ? -num3 : num3;
+        if (absNum1 < absNum2 && absNum1 < absNum3) {
             System.out.println("Наименшее значение: " + num1);
-        } else if ((Math.abs(num2)) < (Math.abs(num1)) && (Math.abs(num2)) < (Math.abs(num3))) {
+        } else if (absNum2 < absNum1 && absNum2 < absNum3) {
             System.out.println("Наименшее значение: " + num2);
         } else {
             System.out.println("Наименшее значение: " + num3);

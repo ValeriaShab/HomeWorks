48bac81cd77b1178f4a165e0561eb28f53d3f980
ValeriaShab <sabaturavaleria@gmail.com>
2023-01-07 19:47:12 +0200

    hw12 corrected

--- src/com/shabatura/hw12/Task3.java
+++ src/com/shabatura/hw12/Task3.java
@@ -2,36 +2,40 @@ package com.shabatura.hw12;
 
 public class Task3 {
     static class Exception1 extends Exception{
+        public Exception1(String message){
+            super(message);
+        }
     }
     static class Exception2 extends Exception{
+        public Exception2(String message){
+            super(message);
+        }
     }
     static class Exception3 extends Exception{
+        public Exception3(String message){
+            super(message);
+        }
     }
 
     static class Thrower{
-        void methodThrower() throws Exception1, Exception2, Exception3{
-            try {
-                throw new Exception1();
-            } catch (Exception1 exception1){
-                exception1.printStackTrace();
+        void methodThrower(int number) throws Exception1, Exception2, Exception3{
+            if(number == 1){
+                throw new Exception1("Ошибка номер 1");
             }
-            try {
-                throw new Exception2();
-            } catch (Exception2 exception2){
-                exception2.printStackTrace();
+            if(number == 2){
+                throw new Exception2("Ошибка номер 2");
             }
-            try {
-                throw new Exception3();
-            } catch (Exception3 exception3){
-                exception3.printStackTrace();
+            if(number == 3){
+                throw new Exception3("Ошибка номер 3");
             }
         }
     }
 
     public static void main(String[] args) {
+        int number = (int) (Math.random()*3);
         Thrower thrower = new Thrower();
         try{
-            thrower.methodThrower();
+            thrower.methodThrower(number);
         } catch (Exception1 | Exception2 | Exception3 exception){
             exception.printStackTrace();
         }
